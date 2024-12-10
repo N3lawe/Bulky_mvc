102c8c13ba392344ae388f1dba581ebac6f89f35
mohannad sroor <mohannadsroor181@gmail.com>
2024-12-10 23:56:13 +0300

    Make A New Project

---
+++ Bulky.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.Web.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+    }
+}
